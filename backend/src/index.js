50c792c879d516d9ec21bea555875d879631340b
Felipe Fonseca <fonsecafel@gmail.com>
2020-01-15 21:23:04 +0100

    Implemented web interface

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes')
 
 const app = express();
 
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(routes);
 
